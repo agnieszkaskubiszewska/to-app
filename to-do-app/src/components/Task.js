b7551158053ddb2d3440968a1ede8d79efa97835
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2024-09-28 22:14:52 +0200

    add new props

--- to-do-app/src/components/Task.js
+++ to-do-app/src/components/Task.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
-const Task = () => {
+const Task = (props) => {
+  const { text, date } = props.task
     return (
-        <h3>Task pojedyńczy</h3>
+        <div>
+          <p>
+            <strong>{text}</strong> - do <span>{date} </span>
+            <button onClick={()=> console.log('Done')} >Zostało zrobione</button>
+            <button onClick={()=> console.log('Deleted')}>X</button>
+          </p>
+        </div>
       );
 }
  
--- to-do-app/src/components/TaskList.js
+++ to-do-app/src/components/TaskList.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Task from './Task';
 
-const TaskList = () => {
+const TaskList = (props) => {
+    const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.changeStatus}/>)
     return (
-        <div>
-            <h1>Lista Tasków</h1>
-            <Task />
-            </div>
+        <>
+        <div className='active'>
+            <h1>Zadania do zrobienia</h1>
+            {tasks}
+        </div>
+            <hr />
+        <div className='done'>
+              <h3>Zadania zrobione (0)</h3>
+              </div>
+        </>
       );
 }
  
