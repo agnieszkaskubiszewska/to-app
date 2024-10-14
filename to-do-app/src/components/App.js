cc3b840b82dbae014cd0af62661c3dab479ec5ca
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2024-10-14 22:59:11 +0200

    done task

--- to-do-app/src/components/App.js
+++ to-do-app/src/components/App.js
@@ -20,12 +20,34 @@ class App extends Component {
         text:"Skończyć malować obraz",
         date:"2024-12-23",
         important:false,
-        active: true, 
+        active: false, 
         finisheDate: null
       },
       {
         id:2,
         text:"Skończyć malować sciane",
+        date:"2024-12-22",
+        important:false,
+        active: true, 
+        finisheDate: null
+      },      {
+        id:3,
+        text:"Skończyć kurs jogi",
+        date:"2024-12-22",
+        important:false,
+        active: true, 
+        finisheDate: null
+      }, 
+      {
+        id:4,
+        text:"Wyrzyc smieci",
+        date:"2024-12-22",
+        important:false,
+        active: true, 
+        finisheDate: null
+      },       {
+        id:5,
+        text:"Kup psa",
         date:"2024-12-22",
         important:false,
         active: true, 
--- to-do-app/src/components/TaskList.js
+++ to-do-app/src/components/TaskList.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Task from './Task';
 
 const TaskList = (props) => {
-    const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.changeStatus}/>)
+    const active = props.tasks.filter(task => task.active)
+    const done = props.tasks.filter(task => !task.active)
+    const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.changeStatus}/>)
+    const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.changeStatus}/>)
     return (
         <>
         <div className='active'>
             <h1>Zadania do zrobienia</h1>
-            {tasks}
+            {activeTasks.length > 0 ? activeTasks : <p>Nic do zrobienia</p>}
         </div>
             <hr />
         <div className='done'>
-              <h3>Zadania zrobione (0)</h3>
+              <h3>Zadania zrobione total <em>{done.length}</em>   </h3>
+              {done.length > 5 && <span style={{fontSize:"10px"}}>Wyswietlamy ostatnie 5 elementow</span>}
+              {doneTasks.slice(0,5)} 
               </div>
         </>
       );
